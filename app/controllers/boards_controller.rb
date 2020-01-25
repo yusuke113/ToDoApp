ed2bd46fe40d5d985ffdedc3ff417eacf762eb10
Your Name <you@example.com>
2020-01-25 20:21:43 +0900

    Add board model

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,27 @@
 class BoardsController < ApplicationController
+
   def index
+    
   end
 
   def new
   end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
+
   root 'boards#index'
+  resources :boards
 
 end
