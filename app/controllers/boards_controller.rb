44b9b0ca81ae97f144c1edb8741ae4ae02741925
Your Name <you@example.com>
2020-02-03 01:05:10 +0900

    Add new create board form

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,13 +1,25 @@
 class BoardsController < ApplicationController
 
   def index
-    
+    @boards = Board.all
+    @board = Board.new
   end
 
   def new
+
   end
 
   def create
+    @board = Board.new(board_params)
+    if @board.save
+      flash[:notice] = "「#{@board.title}」の掲示板を作成しました"
+      redirect_to board
+    else
+      redirect_to new_board_path, flash: {
+        board: @board,
+        error_messages: board.errors.full_messages
+      }
+    end
   end
 
   def edit
@@ -17,10 +29,17 @@ class BoardsController < ApplicationController
   end
 
   def show
+    @board = Board.find(id: params[:id])
   end
 
   def destroy
   end
+
+  private
+
+    def board_params
+      params.require(:board).permit(:title)
+    end
 
 
 
---
+++ app/javascript/packs/boards/new.js
@@ -0,0 +1,38 @@
+'use strict';
+
+{
+	const new_board = $('#new-board-create');
+	const new_board_form = $('#new_board_form');
+	const fade = $('#fade');
+	const board_title_field = $('#board-title-field');
+	const board_create_btn = $('#board-create-btn');
+
+	// <新しいボードを作成>を押したらフォームを表示
+	new_board.click(function() {
+		fade.removeClass('hidden');
+		new_board_form.removeClass('hidden');
+	});
+
+	// ボードタイトル記入欄以外をクリックするとフォームをhiddenする
+	function cancel() {
+		fade.addClass('hidden');
+		new_board_form.addClass('hidden');
+		board_title_field.val('');
+		board_create_btn.removeClass('primary');
+		board_create_btn.addClass('disabled');
+	};
+	$('#window-overlay').click(cancel);
+	$('#cancel').click(cancel);
+
+	// ボードタイトルに入力があれば<button>を<primary>にする
+	board_title_field.on('input', () => {
+		var input_field = board_title_field.val();
+		if (input_field === '') {
+			board_create_btn.removeClass('primary');
+			board_create_btn.addClass('disabled');
+		} else {
+			board_create_btn.removeClass('disabled');
+			board_create_btn.addClass('primary');
+		}
+	});
+}
