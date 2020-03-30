5febd1739146e21d548b27e8bfe5d15875245d9b
Your Name <you@example.com>
2020-03-30 09:52:12 +0900

    Add card create & destroy method ajax

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,19 +4,42 @@ def show
 
 end
 
+# def create
+#   card = Card.new(card_params)
+#   if card.save
+#     redirect_to card.list.board
+#   else
+#     redirect_to card.list.board, alert: 'error'
+#   end
+# end
 def create
-  card = Card.new(card_params)
-  if card.save
-    redirect_to card.list.board
-  else
-    redirect_to card.list.board, alert: 'error'
+  @card = Card.new(card_params)
+  respond_to do |format|
+    if @card.save
+      format.html { redirect_to @card.board }
+      format.json { render :show, status: :created }
+      format.js
+      # redirect_to @list.board
+    else
+      format.html { render :show }
+      format.json { render :show, status: :unprocessable_entity }
+      # render :show, alert: 'error'
+    end
   end
 end
 
 def destroy
-  card = Card.find(params[:id])
-  card.destroy
-  redirect_to card.list.board
+  @card = Card.find(params[:id])
+  respond_to do |format|
+    if @card.destroy
+      format.html { redirect_to @card.board }
+      format.json { render :show, status: :no_content }
+      format.js
+    else
+      format.html { redirect_to @card.board }
+      format.json { render :show, status: :internal_server_error }
+    end
+  end
 end
 
 def update
