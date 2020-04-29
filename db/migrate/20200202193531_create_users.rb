300caa51ee0305aad6f3d3f1f68f4b3ff3f1d378
Your Name <you@example.com>
2020-04-30 00:46:25 +0900

    edit users create migration file

--- db/migrate/20200202193531_create_users.rb
+++ db/migrate/20200202193531_create_users.rb
@@ -1,9 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :name, null: false
-      t.string :email, null: false
-      t.string :password_digest, null: false
 
       t.timestamps
     end
