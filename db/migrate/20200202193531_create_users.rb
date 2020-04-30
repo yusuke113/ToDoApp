10e76585e451e53246d50f8194b3e8027aa1d7ea
Your Name <you@example.com>
2020-04-30 13:31:10 +0900

    delete migration file

--- db/migrate/20200202193531_create_users.rb
+++ db/migrate/20200202193531_create_users.rb
@@ -1,9 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-
       t.timestamps
     end
-    add_index :users, :email, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_151543) do
+ActiveRecord::Schema.define(version: 2020_04_15_120810) do
 
   create_table "boards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", limit: 255
