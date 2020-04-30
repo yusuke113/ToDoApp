5603b9452b6b241ae37e52ff730a4c504a48ebe6
Your Name <you@example.com>
2020-04-30 14:56:09 +0900

    Edit mailer setting

--- config/environment.rb
+++ config/environment.rb
@@ -3,23 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-config.action_mailer.delivery_method = :smtp
-
-config.action_mailer.smtp_settings = {
-
-  :user_name => Rails.application.credentials.sendgrid[:SENDGRID_USERNAME],
-
-  :password => Rails.application.credentials.sendgrid[:SENDGRID_PASSWORD],
-
-  :domain => "herokuapp.com",
-
-  :address => "smtp.sendgrid.net",
-
-  :port => 587,
-
-  :authentication => :plain,
-
-  :enable_starttls_auto => true
-
-}
